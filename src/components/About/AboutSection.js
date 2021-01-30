3f9adae99b8f6118f24512e9e8e24a3cd3cee4cb
Joe Napper <joe.napper@cmisukltd.onmicrosoft.com>
2021-01-30 17:15:53 +0000

    Implemented react typewriter-effect

---
+++ src/components/About/AboutSection.js
@@ -0,0 +1,9 @@
+const AboutSection = () => {
+  return (
+    <div>
+      <h2>Test</h2>
+    </div>
+  );
+};
+
+export default AboutSection;
--- src/components/About/HeroSection.js
+++ src/components/About/HeroSection.js
@@ -3,11 +3,39 @@ import styled from "styled-components";
 // Animation
 import { motion } from "framer-motion";
 import { container, fade } from "../LogoAnimations";
+import Typewriter from "typewriter-effect";
 
 const HeroSection = () => {
   return (
     <HeroContainer variants={container} initial="hidden" animate="show">
-      <motion.h1 variants={fade}>About</motion.h1>
+      <div className="hero-wrapper">
+        <motion.h2 variants={fade}>We</motion.h2>
+        <Typewriter
+          options={{
+            autoStart: true,
+            loop: true,
+          }}
+          onInit={(typewriter) => {
+            typewriter
+              .typeString("Create")
+              .pauseFor(2000)
+              .deleteAll()
+              .typeString("Analyse")
+              .pauseFor(2000)
+              .deleteAll()
+              .typeString("Strategise")
+              .pauseFor(2000)
+              .deleteAll()
+              .typeString("Design")
+              .pauseFor(2000)
+              .deleteAll()
+              .typeString("Develop")
+              .pauseFor(2000)
+              .deleteAll()
+              .start();
+          }}
+        />
+      </div>
     </HeroContainer>
   );
 };
@@ -21,6 +49,30 @@ const HeroContainer = styled(motion.main)`
     rgb(55, 66, 81),
     rgb(30, 37, 45)
   );
+
+  .hero-wrapper {
+    /* position: relative;
+    top: 50%;
+    left: 50%;
+    transform: translate(50%, 50%); */
+    display: flex;
+    height: 100%;
+    /* text-align: center; */
+    justify-content: center;
+    align-items: center;
+    text-transform: uppercase;
+    font-family: "Kollektif";
+  }
+
+  h2 {
+    font-size: 5rem;
+    margin-right: 1rem;
+  }
+
+  .Typewriter {
+    font-size: 5rem;
+    color: #8bd0f2;
+  }
 `;
 
 export default HeroSection;
