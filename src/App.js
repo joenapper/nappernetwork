698bc458fb621909037de5818dfe123db21d8dce
joenapper <joe.napper@hotmail.co.uk>
2022-01-31 15:00:42 +0000

    feat: code splitting

--- src/App.js
+++ src/App.js
@@ -1,58 +1,63 @@
+import { lazy, Suspense } from "react";
 // Components
 import GlobalStyle from "./components/GlobalStyle";
-import Navbar from "./components/Nav/Navbar";
-import FooterSection from "./components/FooterSection";
-// Pages
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Services from "./pages/Services";
-import WebDevelopment from "./pages/WebDevelopment";
-import DigitalMarketing from "./pages/DigitalMarketing";
-import SocialMedia from "./pages/SocialMedia";
-import Contact from "./pages/Contact";
 // Router
 import { Switch, Route } from "react-router-dom";
 // Animations
 import ScrollTop from "./components/ScrollTop";
 
+// Components
+const Navbar = lazy(() => import("./components/Nav/Navbar"));
+const FooterSection = lazy(() => import("./components/FooterSection"));
+// Pages
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
+const Services = lazy(() => import("./pages/Services"));
+const WebDevelopment = lazy(() => import("./pages/WebDevelopment"));
+const DigitalMarketing = lazy(() => import("./pages/DigitalMarketing"));
+const SocialMedia = lazy(() => import("./pages/SocialMedia"));
+const Contact = lazy(() => import("./pages/Contact"));
+
 function App() {
   return (
     <div className="App">
-      <GlobalStyle />
       <ScrollTop />
-      <Navbar />
+      <GlobalStyle />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Navbar />
 
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
 
-        <Route path="/about">
-          <About />
-        </Route>
+          <Route path="/about">
+            <About />
+          </Route>
 
-        <Route path="/services">
-          <Services />
-        </Route>
+          <Route path="/services">
+            <Services />
+          </Route>
 
-        <Route path="/web-development">
-          <WebDevelopment />
-        </Route>
+          <Route path="/web-development">
+            <WebDevelopment />
+          </Route>
 
-        <Route path="/digital-marketing">
-          <DigitalMarketing />
-        </Route>
+          <Route path="/digital-marketing">
+            <DigitalMarketing />
+          </Route>
 
-        <Route path="/social-media">
-          <SocialMedia />
-        </Route>
+          <Route path="/social-media">
+            <SocialMedia />
+          </Route>
 
-        <Route path="/contact">
-          <Contact />
-        </Route>
-      </Switch>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
 
-      <FooterSection />
+        <FooterSection />
+      </Suspense>
     </div>
   );
 }
